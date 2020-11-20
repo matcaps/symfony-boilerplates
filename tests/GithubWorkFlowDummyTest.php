13ed1a7315c1fe89f53ce075c77da3f209170539
spackydev <mathieu.capon@gmail.com>
2020-11-20 09:31:29 +0100

    first-commit

---
+++ tests/GithubWorkFlowDummyTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class GithubWorkFlowDummyTest extends TestCase
+{
+    public function testMyDummyThing()
+    {
+        self::assertTrue(true, "It seems that it is so true that it cannot be false");
+    }
+}
